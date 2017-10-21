e4903c6f1a9c777fa6cb8d76525be7fcf475ae74
Mark Reynoso <mreynoso@spu.edu>
2017-10-21 14:39:39 -0700

    fixed tox

--- setup.py
+++ setup.py
@@ -8,15 +8,10 @@ setup(
     package_dir={'':'src'},
     author='Mark Reynoso',
     author_email='mreynoso@spu.edu',
-    py_modules=['code-katas'],
+    py_modules=['multiples_of_3_and_5', 'changing_letters', 'exclamation_marks_series_6', 'find_the_odd_int', 'list_filtering', 'simple_pig_latin', 'sum_of_two_lowest_possible_integers', 'whats_the_real_floor'],
     install_requires=[],
     extras_require={
         'testing': ['pytest', 'pytest-cov', 'pytest-watch', 'tox'],
         'development': ['ipython']
     },
-    entry_points={
-        'console_scripts': [
-            'code-katas=code-katas:code-katas'
-        ]
-    }
 )
