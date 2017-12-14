31214c974159f6c5531085564cdd5b1eb9afab69
Mark Reynoso <mreynoso@spu.edu>
2017-12-13 17:36:16 -0800

    added index nums for ref

--- src/test_flight_paths.py
+++ src/test_flight_paths.py
@@ -14,3 +14,17 @@ def test_flight_path_returns_correct_path_and_distance():
     output = find_path_between_cities('Juba', 'Khartoum')
     print(output)
     assert output == 1000
+
+
+    Juba: 20
+        Nairobi: 16
+        Kigal:
+        Cairo: 54
+            Nairobi: 16
+            Asmara: 26
+                Dubai: 394
+                Cairo: 54
+                Khartoum: 78
+            Aswan:
+        Dubai: 394
+
