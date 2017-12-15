ac46a6086b5d68031b730a593b6f8279f561f261
Mark Reynoso <mreynoso@spu.edu>
2017-12-14 21:53:27 -0800

    working on bugs

---
+++ src/parenthetics.py
@@ -0,0 +1,33 @@
+"""Assess parenthesis in string."""
+from linked_list import LinkedList
+
+
+def proper_parenthetics(string):
+    """Assess parenthesis in string."""
+    if not isinstance(string, str):
+        raise TypeError('Please enter a unicode string.')
+    char_list = LinkedList()
+    for letter in string:
+        char_list.push(letter)
+    count = 0
+    total = 0
+    broken = False
+    current = char_list.head
+    while current:
+        if current.data == '(':
+            count -= 1
+            total += 1
+            if count < 0:
+                broken = True
+        elif current.data == ')':
+            count += 1
+            total += 1
+        current = current.next
+    if total == 0:
+        return 'There are no parenthesis in this string.'
+    elif total % 2 > 0:
+        return 1
+    elif total % 2 == 0 and broken is True:
+        return -1
+    else:
+        return 0
---
+++ src/test_parenthetics.py
@@ -0,0 +1,51 @@
+"""Test proper parenthetics."""
+from parenthetics import proper_parenthetics
+
+import pytest
+
+
+def test_proper_parenthetics_raises_type_error_if_not_string():
+    """Test proper parenthetics raises type error."""
+    with pytest.raises(TypeError):
+        proper_parenthetics(12345)
+
+
+def test_proper_parenthetics_returns_error_msg_if_no_parens():
+    """Test proper parens returns error msg if no parens in string."""
+    out = proper_parenthetics('string')
+    assert out == 'There are no parenthesis in this string.'
+
+
+def test_proper_parens_returns_zero_for_balanced_string():
+    """Test proper parens if balanced."""
+    assert proper_parenthetics('(())') == 0
+
+
+def test_proper_parents_returns_neg_if_broken():
+    """Test proper parens returns -1 if broken parens."""
+    assert proper_parenthetics('))((') == -1
+
+
+def test_proper_parens_returns_one_if_unclosed():
+    """Test proper parens returns one if unclosed."""
+    assert proper_parenthetics('(()))') == 1
+
+
+def test_proper_parens_balanced_with_letters():
+    """Test proper parens when balanced has letters."""
+    assert proper_parenthetics('(bala(n)ced())') == 0
+
+
+def test_proper_parens_broken_with_letters():
+    """Test proper parens when broken has letters."""
+    assert proper_parenthetics('(bala(n)c)(ed()))(') == -1
+
+
+def test_proper_parens_unclosed_with_letters():
+    """Test proper parens when unclosed has letters."""
+    assert proper_parenthetics('(bala(n)c)(ed()') == 1
+
+
+def test_proper_parens_all_left_parens():
+    """Test proper parens if all parens are left."""
+    assert proper_parenthetics('((((((') == 1
